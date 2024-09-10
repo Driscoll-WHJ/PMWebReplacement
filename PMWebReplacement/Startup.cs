c455244d5204c03ea63aaf2a95042835d63173d0
Wahid Joudrey <Wahid.Joudrey@dchstx.org>
2024-09-10 14:11:40 -0500

    Add project files.

---
+++ PMWebReplacement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PMWebReplacement.Startup))]
+namespace PMWebReplacement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
